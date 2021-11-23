2290f143b4b411437e45b90f5c9f8633da5ef324
Gilang Ramadhan <92220215+GilangRamadhann@users.noreply.github.com>
2021-11-23 18:38:53 +0700

    Add files via upload

---
+++ 2117051082_Gilang Ramadhan_PostTest1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string Rama;
+	string Dea;
+
+	getline (cin, Rama);
+	getline (cin, Dea);
+
+	cout<<endl;
+	cout<<"#############################PERCAKAPAN SINGKAT#############################"<<endl;
+	cout<< "Rama	: " << "Hai salam kenal namaku \"Rama\", nama kamu siapa?" <<endl;
+	cout<< "Dea	: " << "Hai salam kenal juga namaku \"Dea\"" <<endl;
+
+	
+	return 0;
+}
